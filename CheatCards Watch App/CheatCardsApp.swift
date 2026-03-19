595071c86c817d12b20118e5e6ad9883bd0751e1
extrafa <rostislavpredein@gmail.com>
2026-03-19 00:26:56 -0300

    Initial Commit

---
+++ CheatCards Watch App/CheatCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheatCardsApp.swift
+//  CheatCards Watch App
+//
+//  Created by Ross on 19/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct CheatCards_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
